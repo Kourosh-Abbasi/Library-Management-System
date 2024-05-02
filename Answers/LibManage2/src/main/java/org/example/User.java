3e135e22c9b1650c48628c0b2ef354929865ec3e
Kourosh-Abbasi <strangeforever83@gmail.com>
2024-05-02 22:44:23 +0330

    project updated to last version

--- Answers/LibManage2/src/main/java/org/example/User.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-
-public class User extends MainUser{
-    public User(String name, String ID, String phoneNumber, String registerDate) {
-        super(name, ID, phoneNumber, registerDate);
-    }
-}
